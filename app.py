fd53a4d906aafaf965744999b13106becdd7f721
sanjana-pannala <sanjanapannala77@gmail.com>
2026-09-16 10:16:46 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template, request 
+app = Flask(__name__) 
+@app.route('/') 
+def home(): 
+    return ''' 
+    <h2><center>Hello</center></h2> 
+    <center> 
+        <a href="/register">Go to Registration Page</a> 
+    </center> 
+    ''' 
+@app.route('/register') 
+def register(): 
+    return render_template('register.html') 
+@app.route('/success', methods=['POST']) 
+def success(): 
+    name = request.form['name'] 
+    year = request.form['year'] 
+    return render_template('success.html', name=name, year=year) 
+if __name__ == '__main__':
+    app.run(debug=True)
